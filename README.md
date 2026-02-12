13170e65b6f973873be651ddf576749e3dfd1084
Jae <gohyishen777@gmail.com>
2026-02-12 11:47:19 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### 50.051 Programming Language Concept - Parser Project
+
+_This documents the project done for the module 50.051 Programming Language Concept_
+
+---
---
+++ template.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h> //strings
+#include "max.h"
+#include <stdlib.h> //malloc
+#include <math.h>
+#include <time.h>
+
+// Cannot use for project
+#include <ctype.h> //isalpha(), isdigit(), toupper() etc
+
+/*
+If you just have 1 file, run using the play button
+If you using max.c max.h, use this:S
+gcc nameoffile.c -o output/nameoffile.exe
+output nameoffile.exe
+
+*/
+int main(void)
+{
+
+    return 0;
+}
